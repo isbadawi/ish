703ea34fd9879120a5069684cdeb804d7d592ad0
Ismail Badawi <ismail@badawi.io>
2015-04-24 21:23:27 -0400

    Allow reading script files.

--- main.c
+++ main.c
@@ -11,48 +11,52 @@
 
 #include "builtin.h"
 
-char *ish_getline(void) {
-  char *line = NULL;
+int ish_getline(FILE *fp, char **line) {
   size_t n = 0;
-  getline(&line, &n, stdin);
+  *line = NULL;
+  int rc = getline(line, &n, fp);
+  if (rc < 0) {
+    return rc;
+  }
 
   // Strip newline...
-  line[strlen(line) - 1] = '\0';
+  (*line)[strlen(*line) - 1] = '\0';
 
   // Strip comments (but not inside quotes)
   int squote = 0;
   int dquote = 0;
-  for (int i = 0; line[i]; ++i) {
-    if (line[i] == '"' && !squote) {
+  for (int i = 0; (*line)[i]; ++i) {
+    if ((*line)[i] == '"' && !squote) {
       dquote = !dquote;
-    } else if (line[i] == '\'' && !dquote) {
+    } else if ((*line)[i] == '\'' && !dquote) {
       squote = !squote;
-    } else if (line[i] == '#' && !squote && !dquote) {
-      line[i] = '\0';
+    } else if ((*line)[i] == '#' && !squote && !dquote) {
+      (*line)[i] = '\0';
       break;
     }
   }
 
   // If it's empty, bail out.
-  if (!*line) {
-    free(line);
-    return NULL;
+  if (!**line) {
+    free(*line);
+    *line = NULL;
+    return 0;
   }
 
   // If it's all whitespace, bail out.
   int allspace = 1;
-  for (int i = 0; line[i]; ++i) {
-    if (!isspace(line[i])) {
+  for (int i = 0; (*line)[i]; ++i) {
+    if (!isspace((*line)[i])) {
       allspace = 0;
       break;
     }
   }
   if (allspace) {
-    free(line);
-    return NULL;
+    free(*line);
+    *line = NULL;
   }
 
-  return line;
+  return 0;
 }
 
 struct ish_command_t {
@@ -168,12 +172,26 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  while (1) {
+  FILE *fp = stdin;
+  if (optind < argc) {
+    fp = fopen(argv[optind], "r");
+    if (!fp) {
+      perror(argv[optind]);
+      return 1;
+    }
+  }
+
+  char *line;
+  if (fp == stdin) {
     printf("ish$ ");
-    char *line = ish_getline();
+  }
+  while (ish_getline(fp, &line) >= 0) {
     if (line) {
       ish_eval(line);
       free(line);
+    }
+    if (fp == stdin) {
+      printf("ish$ ");
     }
   }
   return 0;
